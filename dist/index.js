42b67de460c497554ca70fb7fd187262befb42e1
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-02-05 11:40:16 +0200

    remove folder dist from gitignore

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+import { concatination } from "./concatenation";
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+if (button && input) {
+    button.addEventListener("click", () => concatination(input.value, "hello"));
+}
+//# sourceMappingURL=index.js.map
